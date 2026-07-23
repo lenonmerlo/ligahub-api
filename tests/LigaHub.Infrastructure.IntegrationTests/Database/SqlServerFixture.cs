af440857b56c8adf4654fe4d9402237756e6732a
lenonmerlo <lenontm@gmail.com>
2026-07-23 08:54:25 -0300

    test(persistence): cover organization repository

---
+++ tests/LigaHub.Infrastructure.IntegrationTests/Database/SqlServerFixture.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using Testcontainers.MsSql;
+
+namespace LigaHub.Infrastructure.IntegrationTests.Database;
+
+public sealed class SqlServerFixture : IAsyncLifetime
+{
+    private readonly MsSqlContainer _container =
+        new MsSqlBuilder(
+            "mcr.microsoft.com/mssql/server:2022-latest")
+        .Build();
+
+    public string ConnectionString
+    {
+        get
+        {
+            var builder = new SqlConnectionStringBuilder(
+                _container.GetConnectionString())
+            {
+                InitialCatalog = "LigaHubTests"
+            };
+
+            return builder.ConnectionString;
+        }
+    }
+
+    public Task InitializeAsync()
+    {
+        return _container.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return _container.DisposeAsync().AsTask();
+    }
+}
