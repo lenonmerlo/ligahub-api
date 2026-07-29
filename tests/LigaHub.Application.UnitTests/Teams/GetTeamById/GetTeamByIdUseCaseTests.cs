c70a5d73991e2dd0ff0ce8ca2bbc8c54f3e389b2
lenonmerlo <lenontm@gmail.com>
2026-07-29 17:03:38 -0300

    test(teams): cover get team by id use case

---
+++ tests/LigaHub.Application.UnitTests/Teams/GetTeamById/GetTeamByIdUseCaseTests.cs
@@ -0,0 +1,65 @@
+﻿using LigaHub.Application.Teams.GetTeamById;
+using LigaHub.Domain.Teams;
+
+namespace LigaHub.Application.UnitTests.Teams.GetTeamById;
+
+public sealed class GetTeamByIdUseCaseTests
+{
+    [Fact]
+    public async Task Execute_ShouldReturnTeam_WhenTeamExists()
+    {
+        var organizationId = Guid.NewGuid();
+        var team = Team.Create(
+            organizationId,
+            "Time Regional");
+        var repository = new FakeTeamRepository
+        {
+            TeamToReturn = team
+        };
+        var useCase = new GetTeamByIdUseCase(repository);
+        var query = new GetTeamByIdQuery(
+            organizationId,
+            team.Id);
+
+        var result = await useCase.ExecuteAsync(query);
+
+        Assert.NotNull(result);
+        Assert.Equal(team.Id, result.Id);
+        Assert.Equal(organizationId, result.OrganizationId);
+        Assert.Equal(team.Name, result.Name);
+        Assert.Equal(
+            organizationId,
+            repository.RequestedOrganizationId);
+        Assert.Equal(team.Id, repository.RequestedId);
+    }
+
+    [Fact]
+    public async Task Execute_ShouldReturnNull_WhenTeamDoesNotExist()
+    {
+        var repository = new FakeTeamRepository();
+        var useCase = new GetTeamByIdUseCase(repository);
+        var query = new GetTeamByIdQuery(
+            Guid.NewGuid(),
+            Guid.NewGuid());
+
+        var result = await useCase.ExecuteAsync(query);
+
+        Assert.Null(result);
+        Assert.Equal(
+            query.OrganizationId,
+            repository.RequestedOrganizationId);
+        Assert.Equal(query.Id, repository.RequestedId);
+    }
+
+    [Fact]
+    public async Task Execute_ShouldThrowArgumentNullException_WhenQueryIsNull()
+    {
+        var repository = new FakeTeamRepository();
+        var useCase = new GetTeamByIdUseCase(repository);
+
+        var exception = await Assert.ThrowsAsync<ArgumentNullException>(
+            () => useCase.ExecuteAsync(null!));
+
+        Assert.Equal("query", exception.ParamName);
+    }
+}
